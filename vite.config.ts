8c39cfafe2abf6c0bbf3fbf712726a1fa8340eb1
Knicklichtjedi <knicklichtjedi+dev@proton.me>
2026-06-12 22:42:43 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     build: {
         lib: {
             entry: path.resolve(__dirname, 'src/main.ts'),
-            name: 'sf2e-auto-action-tracker',
+            name: 'pf2e-auto-action-tracker',
             formats: ['es'],
             fileName: 'main'
         },
